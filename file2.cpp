b3b7da759d0a384a10c37ba67c82c30a4dcd9190
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-10 20:59:43 +0700

    menambahkan string status

--- file2.cpp
+++ file2.cpp
@@ -4,3 +4,14 @@ using namespace std;
 float Rata_rata(float a, float b){
     return (a+b)/2;
 }
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else if (r >= 50)
+        return "Lolos dipertimbangkan";
+    else if (r >= 40)
+        return "Lolos tetapi matrikulasi 1 tahun";
+    else 
+        return "Gagal";
+}
