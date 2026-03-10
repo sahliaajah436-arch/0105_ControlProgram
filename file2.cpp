8767be6f1c25e91d9a80b8523c40f3164fce6fc4
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-10 21:01:36 +0700

    menambahkan status nilai

--- file2.cpp
+++ file2.cpp
@@ -15,3 +15,20 @@ string Status_lulus(float r){
     else 
         return "Gagal";
 }
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout <<"Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout <<"Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    //rerata + (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //     status = "Lolos";
+    //else
+        // status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
